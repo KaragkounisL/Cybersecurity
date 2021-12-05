34a26eab6c348ad18d39140f7480201817daeaa4
Leonidas Karagkounis <60888243+KaragkounisL@users.noreply.github.com>
2021-12-05 12:12:48 +0200

    Update urlresolve.sh

--- urlresolve.sh
+++ urlresolve.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 for hostname in $(cat urls.txt);do
-host $hostname |grep "has address"|cut -d" " -f4 |sort -u &
+host $hostname | grep "has address" | cut -d " " -f 4 |sort -u &
 done > urlips.txt
 
